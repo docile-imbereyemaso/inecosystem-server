6a30dfb0764ceed75cf69a528bbed678365df54f
Docile123 <imbereyemasoo@gmail.com>
2025-09-05 18:08:07 +0200

    fix: unknown package

--- routes/ineco.routes.js
+++ routes/ineco.routes.js
@@ -1,14 +1,6 @@
-/**
- * This file defines the routes related to student operations.
- * It currently includes a route to get all students.
- *
- * Add more routes for creating, updating, and deleting students as needed.
- */
+
 import express, { Router } from "express";
 
-
-
-import { deleteStudent, getAllStudents,updateStudent,searchbyname, getStudent} from "../controllers/ineco.controller.js";
 
 
 import {insertCompany,insertContribution,getContribution, insertInternship, insertJob, getCompanies, getJobs,getInternships, insertInsight,getInsights} from "../controllers/companies.controller.js";
