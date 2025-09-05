cabe999daea4848beb5cd4ee8e4c2bc17d9e9b86
Docile123 <imbereyemasoo@gmail.com>
2025-09-05 17:59:35 +0200

    ft:bcrypt

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ import cors from "cors";
 
 
 const app = express();
-// Allow all origins (for development)
 app.use(cors());
-
-// Or more selectively:
-// app.use(cors({ origin: "http://localhost:5173" }));
-
-// app.use(express.json());
 app.use(express.json());
 
+
 app.use("/api", companyRoutes);
+app.use("/",(req,res)=>{
+    return res.json({message:"Welcome Inecosystem Bridge"})
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
